5cc8640aa87df894d8930eec88a6cc79c70877c7
Christina Ruc <Christina Ruc>
2017-01-26 21:41:47 -0500

    shiz.js file

---
+++ shiz.js
@@ -0,0 +1,36 @@
+//initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAzbTjPtur9yPmVMU1bFKabRQGm7XDPaCk",
+    authDomain: "mechanic-locator.firebaseapp.com",
+    databaseURL: "https://mechanic-locator.firebaseio.com",
+    storageBucket: "mechanic-locator.appspot.com",
+    messagingSenderId: "919713038261"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//button onclick event
+$("#button").on("click", function(event){
+  event.preventDefault();
+
+  var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=auto+repair+near+charlotte&location=35.2033529,-80.980258&radius=100&key=AIzaSyBvZ6mmr-zE_3pX8xlvcmGxkH6GVVG4WBk"
+
+//ajax call function need the api to complete
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+//
+.done(function(response){
+  
+  var results = response.data;
+ //create a new div for the results
+  var locationDiv = $("<div class=searchResults>");
+//create a place for the list on the left side  
+  locationDiv.replaceWith(ul); //list
+ //create a place for the map on the right side 
+  locationDiv.replaceWith(img); //map
+
+});
